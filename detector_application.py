597456431d5a6885c62bc0cecdd522290b25df76
EduardoVaca <eduardo.vaca.g@gmail.com>
2017-11-10 13:02:29 -0600

    Add intro

--- detector_application.py
+++ detector_application.py
@@ -1,16 +1,25 @@
 import tkinter as tk
 
 BG_COLOR = 'black'
+FONT_COLOR = 'white'
+HEIGHT = 500
+WIDTH = 500
 ROOT = tk.Tk()
 
 def configure_root():
     ROOT.title('Ich connais esa lied, I credo')
-    ROOT.geometry('500x500')
+    ROOT.geometry('{}x{}'.format(WIDTH, HEIGHT))
     ROOT.resizable(0, 0)
     ROOT.configure(background=BG_COLOR)
 
 def create_ui_elements():    
-    tk.Label(ROOT, text='Ich connais esa lied, I credo', fg = 'white', bg=BG_COLOR, font = "Helvetica 20 bold").pack()
+    tk.Label(ROOT, text='Ich connais esa lied, I credo', fg=FONT_COLOR, bg=BG_COLOR, font='Avenir 20 bold').pack()
+    author = tk.Message(ROOT, text='by Eduardo Vaca')
+    author.config(fg=FONT_COLOR, bg=BG_COLOR, font='Avenir 10', width=200)
+    author.pack()
+    intro = tk.Message(ROOT, text='\nThis project allows you to predict the language of a song based on a Logistic Regression model')
+    intro.config(fg=FONT_COLOR, bg=BG_COLOR, font='Avenir 14', width=WIDTH-20)
+    intro.pack()
 
 def run():
     ROOT.mainloop()
